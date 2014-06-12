065d0ddba16bbbb0a77d1710afaea91f6c0c0990
ckevinliu <Kevin.chieh.liu@gmail.com>
2014-06-12 16:47:26 -0700

    Update user.rb

--- user.rb
+++ user.rb
@@ -5,9 +5,5 @@ class User
   def initialize(user_input)
     @user_input = user_input
   end
-
-  # def check_user(user_input)
-  # 	return "Hello, #{user_input}! That's long." if user_input.length >= 10
-  # 	return "Hello, #{user_input}!"
-  # end
+  
 end
